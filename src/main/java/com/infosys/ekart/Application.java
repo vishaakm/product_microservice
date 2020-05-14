5c1d991a3393a6fdc7f0e93d98aab3ea66fba5f9
Vishaak M <vishaak.m@gmail.com>
2020-05-14 20:10:46 +0530

    Update

--- src/main/java/com/infosys/ekart/Application.java
+++ src/main/java/com/infosys/ekart/Application.java
@@ -16,8 +16,10 @@ import java.util.Properties;
 public class Application {
     public static Properties PROP;
     public static void main(String[] args) {
+        System.out.println("Main Start");
         PROP = readPropertiesFromS3();
         SpringApplication.run(Application.class, args);
+        System.out.println("Main End");
     }
 
     public static Properties readPropertiesFromS3() {
@@ -26,9 +28,13 @@ public class Application {
         String bucket_name = "properties-bucket";
         Properties prop = new Properties();
 
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion("us-east-2").build();
+//        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion("us-east-2").build();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+
         S3Object object = s3.getObject(new GetObjectRequest(bucket_name, key_name));
         try {
+            System.out.println("Fetch Data");
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(object.getObjectContent()));
             String line;
             while ((line = reader.readLine()) != null) {
@@ -37,6 +43,7 @@ public class Application {
             }
 
             object.close();
+            System.out.println("Fetch End");
         } catch (Exception e) {
             e.printStackTrace();
         }
